a9f6bbe631933e67ddfbe0a934203a48321b757c
psysinner <psychosinner@proton.me>
2025-10-01 03:44:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Telegram API credentials (get from my.telegram.org)
-API_ID = "123456"
-API_HASH = "hash here"
-BOT_TOKEN = "bot token here"
+API_ID = "25489830"
+API_HASH = "b3df97daea65252260b2c2513a94ee5a"
+BOT_TOKEN = "8230766671:AAFl96_m6TgmdjverB1arDToO09rRLyVv78"
 
 # Rate limiting settings (in seconds)
 ADMIN_DELAY = 1.5      # Delay between admin checks
@@ -22,5 +22,5 @@ LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 ADMIN_IDS = [6082136901]
 FORCE_SUB_CHANNEL_ID = "-1002581367215"
 # MongoDB configuration
-MONGODB_URI = '' # or your MongoDB connection string
-DATABASE_NAME = 'autoposter_bot'
+MONGODB_URI = 'mongodb+srv://valentina:valentina@cluster0.igx2yx7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' # or your MongoDB connection string
+DATABASE_NAME = 'autopost'
